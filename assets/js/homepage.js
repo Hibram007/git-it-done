e6b24c17811256c8a2954306ef1eef619a905d58
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-01 18:02:11 -0700

    HTTP request hardcoded

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,17 @@
-// fetches data from a server side api - from this github api that commincates to data base
-var getUserRepos = function() {
-   fetch("https://api.github.com/users/octocat/repos");
+
+// created variable to house function to get user data from a web api
+var getusersRepos = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        //formating returned data into json and then storing formated data
+        response.json().then(function(data) {
+            //loggin formated json data into the consol = array displayed
+            console.log(data);
+        });
+    });
 };
 
-//this will call function to console log "function was called"
-getUserRepos();
-
-
+getusersRepos('facebook')
