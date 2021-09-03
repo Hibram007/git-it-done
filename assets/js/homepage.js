1a30993ff3e3dbe9b021cc396d038a1b1a71f1a8
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-03 14:23:49 -0700

    code to display data on page inserted

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+// defined variables
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 // created variable to house function to get user data from a web api
 var getusersRepos = function(user) {
@@ -6,12 +9,90 @@ var getusersRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        //formating returned data into json and then storing formated data
-        response.json().then(function(data) {
-            //loggin formated json data into the consol = array displayed
-            console.log(data);
-        });
-    });
+        // when request is successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            displayRepos(data, user);
+          });
+          //if no user is found - input does not match any users
+        } else {
+          alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
+      });
+}
+
+// variable for the entire form
+var userFormEl = document.querySelector("#user-form");
+
+// variable for the form's input data
+var nameInputEl = document.querySelector("#username");
+
+// variable to house event handler- preventing browser default behaviour= refreshing at submission
+var fromSubmitHandler = function(event) {
+    event.preventDefault();
+// get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+    getusersRepos(username);
+    nameInputEl.value = "";
+} else {
+    alert("Please enter a Github user");
+}
+    console.log(event);
 };
 
-getusersRepos('facebook')
+var displayRepos = function(repos, searchTerm) {
+    // if statment that will tell user that user is found but no repos
+ if ( repos.length === 0) {
+     repoContainerEl.textContent = "No repositories found.";
+     return;
+ }
+    console.log(repos);
+    console.log(searchTerm);
+
+// clear old content
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent = searchTerm;
+
+// loop over repos
+for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+  
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+  
+    // append to container
+    repoEl.appendChild(titleEl);
+  
+   // create a status element - track which repos need help
+var statusEl = document.createElement("span");
+statusEl.classList = "flex-row align-center";
+
+// check if current repo has issues or not
+if (repos[i].open_issues_count > 0) {
+  statusEl.innerHTML =
+    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+} else {
+  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+}
+
+// append to container
+repoEl.appendChild(statusEl);
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+// adding a submit event handler to entire dynamic form element
+userFormEl.addEventListener("submit", fromSubmitHandler);
